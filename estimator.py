ab1cc1f42ef59f6051d50bec5ebd15dfdf2da95d
doowybbob <doowybbob>
2015-06-21 10:35:57 -0400

    Now accounts for retraction lengths. Also does not double count the last E length if the file ends in a G92

--- estimator.py
+++ estimator.py
@@ -4,27 +4,43 @@ import re
 import sys
 
 def estimateFilament (filename):
-	#filename = '3mmBox.gcode'
-
 	gcode_file = open(filename, 'r')
 
 	total_mm = 0
 	running_total = 0
 
+	absolute = True
+
 	for line in gcode_file:
 		#line = gcode_file.readline()
 		if ("G92 " in line):
 			total_mm += float(running_total)
-			
+			#print "total",total_mm
+			running_total = 0
+		
+		elif ("G91 " in line):
+			absolute = False	
+			total_mm += float(running_total)
+			running_total = 0
+		
+		elif ("G90 " in line):
+			absolute = True
+		
 		elif ("G1 " in line):
-			results = re.search("E(\d+\.\d+)", line)
+			results = re.search("E(-?\d+(\.\d+)?)", line)
+			#results = re.search("E(\d+\.\d+)", line)
 			if (results != None):
-				running_total = results.group(1)
+				if (absolute):
+					running_total = results.group(1)
+					#print running_total
+				else:
+					total_mm += float(results.group(1))
 
+	#print running_total
 	total_mm += float(running_total)
 
 	gcode_file.close()
-	return round(total_mm)
+	return round(total_mm, 1)
 
 def main(args):
 	toString = "----------\nEstimated length of filament for: \n"
