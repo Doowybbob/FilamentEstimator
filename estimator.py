94337a1e8326707cb7b8611480d475dc7e27bff2
Bobby <robert.bobby.wood@gmail.com>
2015-06-21 11:43:06 -0400

    Added documentation. Cleaned code. Added help message when no args are displayed. Handles file not found exceptions nicely.

--- estimator.py
+++ estimator.py
@@ -3,51 +3,76 @@
 import re
 import sys
 
+# Estimate the length of filament required for a given gcode file
 def estimateFilament (filename):
-	gcode_file = open(filename, 'r')
-
 	total_mm = 0
 	running_total = 0
 
 	absolute = True
 
+	#Try to open the file
+	try:
+		gcode_file = open(filename, 'r')
+	except: #Handle the error nicely if the file cannot be opened.
+		return "File Not Found"
+	
+	# read the gcode file line by line
 	for line in gcode_file:
-		#line = gcode_file.readline()
-		if ("G92 " in line):
+		if ("G92 " in line): 
+			#The E value will be reset here so add the running total
+			#length to the total length and reset the running length.
 			total_mm += float(running_total)
-			#print "total",total_mm
 			running_total = 0
 		
 		elif ("G91 " in line):
+			#E values will now be relative and not absolute
 			absolute = False	
-			total_mm += float(running_total)
+			total_mm += float(running_total) # add the running length and then reset it to 0
 			running_total = 0
 		
 		elif ("G90 " in line):
+			#E values will now be absolute
 			absolute = True
 		
 		elif ("G1 " in line):
-			results = re.search("E(-?\d+(\.\d+)?)", line)
-			#results = re.search("E(\d+\.\d+)", line)
+			#get the E values. Examples: E-1, E25, E0.535 will all be matched
+			results = re.search("E(-?\d+(\.\d+)?)", line) # This will find E values that are negative and optionally have decimals
 			if (results != None):
 				if (absolute):
+					#If the E values are absolute, we just want to find the last one
+					#so update the running total
 					running_total = results.group(1)
-					#print running_total
 				else:
+					#If the E values are relative, then their values should be added or 
+					#subtracted from the total length so far.
 					total_mm += float(results.group(1))
 
-	#print running_total
+	#add and lengths that haven't already been added
 	total_mm += float(running_total)
 
 	gcode_file.close()
-	return round(total_mm, 1)
+	return round(total_mm, 1) # return the length in mm to one decimal point
 
 def main(args):
+	
 	toString = "----------\nEstimated length of filament for: \n"
+	
+	#find the length of each file individually and make its output nice.
 	for filename in args:
 		length = estimateFilament(filename)
 		toString = toString + filename +  " --> " + str(length) + " mm\n"
+	
 	toString = toString + "----------"
 	return toString
 
-print main(sys.argv[1:])
+if (len(sys.argv) == 1):
+	print "Please provide at least one gcode file as an argument."
+	print "Multiple gcode files can be processed at once. Provide each file as a separate argument"
+	print "\n\tExample: python estimator.py Sample1.gcode Sample2.gcode"
+	print "\nThis will process both Sample1.gcode and Sample2.gcode"
+else:
+	#We assume that all arguments are gcode files. 
+	#The first arg in the argv array will always be the script name, so we
+	#don't want it to be parsed.
+	print main(sys.argv[1:])
+
